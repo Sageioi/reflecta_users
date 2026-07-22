e6dd5517a3eb635dfe5cdabf520653ef72c7576e
Isikuru Oghenetega <tegaisikuru@gmail.com>
2026-07-22 08:38:45 -0900

    Fixed some issues with the font

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ const App = () => {
     return (
         <div className='w-full h-screen  bg-white flex justify-center align-middle'>
             <div className='w-400 h-100 grid grid-cols-1 text-6xl '>
-                <div className=' flex justify-center items-center bg-purple-600 m-2 p-4'><span className='tracking-tight font-medium text-white'>The Aftermath</span></div>
-                <div className=' flex justify-center items-center '><span className='tracking-tight font-medium text-purple-600'>{users} Reflectans have signed up and started using the app.</span></div>
+                <div className=' flex justify-center items-center bg-purple-600 m-2 p-4'><span className='tracking-wide font-medium text-white'>The Aftermath</span></div>
+                <div className=' flex justify-center items-center '><span className='font-medium text-purple-600'>{users} Reflectans have signed up and started using the app.</span></div>
                 <div className=' flex justify-center align-middle'><img src = {reflecta_logo} className='transform-content  delay-100 hover:-translate-y-2.5 transition-all shadow-2xl leading-2 ring-1 ring-purple-600 m-2  rounded-full h-40 w-40'/></div>
             </div>
         </div>
